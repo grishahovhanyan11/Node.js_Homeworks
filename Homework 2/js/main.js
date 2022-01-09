8ce61a72d9ebec0c60c3b36dfe3f8e41f9f63b2e
Grisha Hovhanyan <grisha.hovhanyan@ysumail.am>
2022-01-09 22:53:59 +0400

    reverseButtonClicked function now take the argument obj

--- Homework 2/js/main.js
+++ Homework 2/js/main.js
@@ -2,4 +2,4 @@ let alertButton = document.getElementById("alertObjButton");
 let objReverseButton = document.getElementById("taskObjReverse");
 
 alertButton.addEventListener('click', () => alert(JSON.stringify(obj, undefined, 2)));
-objReverseButton.addEventListener('click', reverseButtonClicked);
+objReverseButton.addEventListener('click', () => { reverseButtonClicked(obj) });

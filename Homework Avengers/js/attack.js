9ba991214e484acfce2e77447873d3854bab418b
Grisha Hovhanyan <grisha.hovhanyan@ysumail.am>
2022-01-17 22:43:18 +0400

    demo avengers

---
+++ Homework Avengers/js/attack.js
@@ -0,0 +1,84 @@
+//[char] at short of [character]
+function removeChar(charactersArr, char) {
+  //remove [char] from [charactersArr]
+  let charIndex = charactersArr.indexOf(char);
+  let temp = charactersArr[charactersArr.length - 1];
+  charactersArr[charactersArr.length - 1] = charactersArr[charIndex];
+  charactersArr[charIndex] = temp;
+
+  charactersArr.length--;
+}
+
+function char1AttackChar2(character1, character2) {
+  console.log(
+    `${character1.name}[${character1.health}] hits ${character2.name}[${character2.health}] with power of ${character1.power}`
+  );
+
+  character2.health = +(character2.health - character1.power).toFixed(2);
+}
+
+function findTeamThatWon(teamThatLost) {
+  let teamThatWin;
+  if(teamThatLost === heroes) {
+    teamThatWin = villains;
+    console.log("Villains win.")
+  } else {
+    teamThatWin = heroes;
+    console.log("Heroes win.")
+  }
+
+  for(let winner of teamThatWin) {
+    clearInterval(winner.intervalId);
+    console.log(`${winner.name}[${winner.health}]`);
+  }
+}
+
+function addSetInterval(char, charArrWhichWillBeAttacked) {
+  let attackInterval = (1 / char.speedLevel) * 5;
+
+  let id = setInterval(() => {
+    let randomIndex = randomInteger(0, charArrWhichWillBeAttacked.length - 1);
+    let char2 = charArrWhichWillBeAttacked[randomIndex];
+    char1AttackChar2(char, char2);
+
+    if (char2.health <= 0) {
+      console.log(`${char2.name} died`);
+      if(typeof char2.intervalId === "number") {
+        clearInterval(char2.intervalId);
+      }
+      removeChar(charArrWhichWillBeAttacked, char2);      
+    }
+
+    if(charArrWhichWillBeAttacked.length === 0) {
+      findTeamThatWon(charArrWhichWillBeAttacked)
+    }
+  }, attackInterval);
+
+  char.intervalId = id;
+}
+
+function startAttack(heroes, villains) {
+  for (let i = 0; i < villains.length; i++) {
+    let randomIndex = randomInteger(0, heroes.length - 1);
+    char1AttackChar2(villains[i], heroes[randomIndex]);
+
+    if(heroes[randomIndex].health <= 0) {
+      removeChar(heroes, heroes[randomIndex]);
+    }
+
+    addSetInterval(villains[i], heroes);
+  }
+
+  for (let i = 0; i < heroes.length; i++) {
+    let randomIndex = randomInteger(0, villains.length - 1);
+    char1AttackChar2(heroes[i], villains[randomIndex]);
+
+    if(villains[randomIndex].health <= 0) {
+      removeChar(villains, villains[randomIndex]);
+    }
+
+    addSetInterval(heroes[i], villains);
+  }
+}
+
+startAttack(heroes, villains);
---
+++ Homework Avengers/js/creatCharacters.js
@@ -0,0 +1,42 @@
+let startAttackButton = document.getElementById("startButton");
+
+function randomInteger(max, min) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function randomDecimal(max, min) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+function fillArray(namesArr) {
+  let resultCharacters = [];
+
+  for (let i = 0; i < 10; i++) {
+    let randomIndexForName = randomInteger(0, namesArr.length - 1);
+    let currentHeroObj = {
+      name: namesArr[randomIndexForName],
+      speedLevel: +randomDecimal(1, 5),
+      health: 100,
+      power: +randomDecimal(1, 10)
+    };
+
+    resultCharacters.push(currentHeroObj);
+
+    let temp = namesArr[namesArr.length - 1];
+    namesArr[namesArr.length - 1] = namesArr[randomIndexForName];
+    namesArr[randomIndexForName] = temp;
+
+    namesArr.length--;
+  }
+
+  return resultCharacters;
+}
+
+const heroNames = ["Wolverine", "Spider-Man", "Thor", "Iron-Man", "Hulk",
+  "Captain-America", "Deadpool", "Doctor-Strange", "Black-Panther", "Black-Widow"];
+
+const villainNames = ["Green-Goblin", "Hela", "Mysterio", "Xu-Wenwu", "Doctor-Octopus", 
+  "Magneto", "Loki", "Killmonger", "Thanos", "Vulture"];
+
+let heroes = fillArray(heroNames);
+let villains = fillArray(villainNames);
